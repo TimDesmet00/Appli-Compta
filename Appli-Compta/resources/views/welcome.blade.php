a899b10c6fb6d2abd7d9958c23587c0305307fa0
tim <timdesmet00@gmail.com>
2025-01-15 10:13:50 +0100

    ajustement meta

--- Appli-Compta/resources/views/welcome.blade.php
+++ Appli-Compta/resources/views/welcome.blade.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
 <html lang="fr"> <!--lang= langue utilisée pour la page-->
     <head>
-        <title>Collection Laravel</title>  <!--titre de la page affiché dans l'onglet navigateur-->
+        <title>TimToCompta</title>  <!--titre de la page affiché dans l'onglet navigateur-->
         <meta charset="utf-8"> <!--cryptage de la page pour facilité la lecture par le moteur de recherche-->
-        <meta name="description" content="collection d'animé realiser en laravel">    <!--courte description du contenu de la page-->
-        <meta name="keywords" content="collection, anime, Laravel">  <!--mot cléf pour le moteur de recherche: keyword1, keyword2, ...-->
+        <meta name="description" content="ERP compta pour moi">    <!--courte description du contenu de la page-->
+        <meta name="keywords" content="comptaSimple, ERP, Laravel">  <!--mot cléf pour le moteur de recherche: keyword1, keyword2, ...-->
         <meta name="author" content="Desmet Tim">   <!--auteur de la page: auteur1, auteur2, ...-->
         <meta name="viewport" content="width=device-width, initial-scale=1">  <!--pour garder les proportions sur différent affichage-->
         <meta name="csrf-token" content="{{ csrf_token() }}">
@@ -18,7 +18,7 @@
             
         </header>
         <main>
-            <!-- @yield('content') -->
+            @yield('content')
         </main>
         <footer>
             
